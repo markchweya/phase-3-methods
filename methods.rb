32b287638ff882d759908d11db50fdeb94b32357
Mark <mark.nyambane@student.moringaschool.com>
2023-05-15 20:41:05 +0300

    Complete Assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,25 @@
 # Your code here!
+def greet_programmer 
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}! "
+end
+
+
+
+
+def greet_with_default(name = "Programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(num1, num2)
+  return num1 + num2
+end
+
+
+def halve(num)
+    return num/2 if num.is_a?(Integer)
+    nil
+end
